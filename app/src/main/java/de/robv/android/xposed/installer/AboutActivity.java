6087cd68c6620c60f4ec0702745c72e6794c9ff7
Pavel Sikun <pavel.sikun@gmail.com>
2016-09-04 00:22:07 +0300

    swipeBackLayout done(excl. statusbar transparency)

--- app/src/main/java/de/robv/android/xposed/installer/AboutActivity.java
+++ app/src/main/java/de/robv/android/xposed/installer/AboutActivity.java
@@ -16,7 +16,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.jude.swipbackhelper.SwipeBackHelper;
